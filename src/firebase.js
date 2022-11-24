1b3db86f1729bdb1cab5416ca15d016908fb738f
insectfow <ehdgus441@naver.com>
2022-11-24 15:56:04 +0900

    firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqWii9uzfrAxMkkhC5yh7dVsmmZAXhYOQ",
+  authDomain: "dwitter-c6269.firebaseapp.com",
+  projectId: "dwitter-c6269",
+  storageBucket: "dwitter-c6269.appspot.com",
+  messagingSenderId: "680715065829",
+  appId: "1:680715065829:web:d3615afa20c6f974e0f70c"
+};
+
+export default firebase.initializeApp(firebaseConfig);
