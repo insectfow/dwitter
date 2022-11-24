f9cfc823e032e1a5b03a16cb3640381070c315ba
insectfow <ehdgus441@naver.com>
2022-11-24 17:04:28 +0900

    auth.js update

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 1. npm i firebase
 
 2. firebase.js setting
+
+3. react router dom install
+
+4. firebase auth setting
+
+5. auth.js setting
+
+6. firebase auth login add in auth.js
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChanges = (event) => {
+    console.log(event.target);
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name='email' type='text' placeholder="Email" required alue={email} onChange={onChanges}></input>
+        <input name='password' type='password' placeholder="Password" required value={password} onChange={onChanges} ></input>
+        <input type="submit" value="log in"></input>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  )
+}
+
 
 export default Auth;
